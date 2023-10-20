ec50c7536c75298699d7ecbfcfa0ae9643873be2
Flirm <121270101+Flirm@users.noreply.github.com>
2023-10-20 16:22:00 -0300

    added comments and spacing

--- globalSettings.py
+++ globalSettings.py
@@ -5,10 +5,14 @@ def initPlayer():
     #defines witch direction the player is facing (0 = right, 1 = left)
     global direction
     direction = 0
+
+    #variables to handle jumping
     global jumpDelay
     jumpDelay = False
     global jumpForce
     jumpForce = 100
+
+    #variables to handle gravity
     global grounded
     grounded = True
     global gravForce
